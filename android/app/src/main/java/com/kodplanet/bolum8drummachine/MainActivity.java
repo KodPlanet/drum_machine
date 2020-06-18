8ab4075fabfa8b688cbe5a12fc478c76df3b0b16
Kod Planet <65843836+KodPlanet@users.noreply.github.com>
2020-06-18 16:52:18 +0300

    Add files via upload

---
+++ android/app/src/main/java/com/kodplanet/bolum8drummachine/MainActivity.java
@@ -0,0 +1,6 @@
+package com.kodplanet.bolum8drummachine;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(DrumMachine());
+}
+
+class DrumMachine extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: SafeArea(
+          child: Container(),
+        ),
+      ),
+    );
+  }
+}
